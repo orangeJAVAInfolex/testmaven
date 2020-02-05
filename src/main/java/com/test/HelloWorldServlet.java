204b3b22572f5ae5e0b5b6be4f68e0a535cd4f46
杨程 <cheng.yang@infolex.com.cn>
2020-02-05 23:07:14 +0800

    测试代码提交绑定GITHUB

---
+++ src/main/java/com/test/HelloWorldServlet.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import javax.servlet.http.HttpServlet;
+
+public class HelloWorldServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	  public HelloWorldServlet(){
+		  super();
+	  }
+}
